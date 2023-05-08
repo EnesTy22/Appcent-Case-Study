2f616e7b94a680791808b986db4705021cdc4c9b
SiecleQ <ensylmz1441@gmail.com>
2023-05-08 13:56:57 +0300

    Initial Commit

---
+++ Appcent-Case-Study/Core/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appcent-Case-Study
+//
+//  Created by Enes Talha Yılmaz on 8.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
